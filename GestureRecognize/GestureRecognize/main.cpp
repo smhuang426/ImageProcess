f28319dbefc0cee1089b2b3395676a451547d320
NoaSMHuangh <smhuang426@gmail.com>
2016-07-30 18:12:11 +0800

    [OpenCV] My first gesture recognize project
    Symptom: NA
    Root Cause: NA
    Solution: My firt gesture recognize project
    Project: OpenCV
    Note: NA

---
+++ GestureRecognize/GestureRecognize/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GestureRecognize
+//
+//  Created by 黃聖閔 on 2016/7/3.
+//  Copyright © 2016年 Noah_Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
